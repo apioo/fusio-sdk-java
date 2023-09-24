125184956236124a6dc1e3e7caf75991e4f71955
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-09-24 11:51:57 +0200

    update gen

--- src/test/java/org.fusioproject.sdk/ClientTest.java
+++ src/test/java/org.fusioproject.sdk/ClientTest.java
@@ -1,6 +1,7 @@
 
 package org.fusioproject.sdk;
 
+import app.sdkgen.client.Credentials.OAuth2;
 import app.sdkgen.client.Exception.Authenticator.InvalidCredentialsException;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,7 +10,7 @@ public class ClientTest {
 
     @Test
     public void testClient() throws InvalidCredentialsException {
-        Client client = new Client("https://demo.fusio-project.org/", "test", "FRsNh1zKCXlB");
+        Client client = new Client("https://demo.fusio-project.org/", new OAuth2("test", "FRsNh1zKCXlB", "https://demo.fusio-project.org/authorization/token", ""));
 
         Assert.assertNotNull(client.backend());
         Assert.assertNotNull(client.consumer());
